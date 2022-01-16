d586c641ed92d3c5ec5693fd156b1cea66205b4c
Linh B Ngo <lngo@wcupa.edu>
2022-01-16 10:43:45 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
+# This is a combination of Dockerfile, setup file from the dyn docker, and init_app.sh
+
 R -e 'install.packages("openssl", dep = T, repos="https://archive.linux.duke.edu/cran/", verbose = FALSE)'
 R -e 'install.packages("devtools", dep = T, repos="https://archive.linux.duke.edu/cran/", verbose = FALSE)'
 R -e 'Sys.setenv(GITHUB_PAT = "28415a8c2138cf40e035c7c474add12b5885ceae"); devtools::install_github("dynverse/dynplot", ref = "devel", force = T)'
@@ -27,3 +29,19 @@ do
   echo "******************************************************************************"
 done < requirements.txt
 
+if [ ! -d "data" ]; then
+  # Create dir
+  mkdir data data/example_PBMC
+
+  # Download data
+  wget https://cf.10xgenomics.com/samples/cell/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz
+  tar zxvf pbmc3k_filtered_gene_bc_matrices.tar.gz
+  mv filtered_gene_bc_matrices/hg19/* data/example_PBMC
+  rm -rf filtered_gene_bc_matrices pbmc3k_filtered_gene_bc_matrices.tar.gz
+fi
+
+## RDS_files
+if [ ! -d "RDS_files" ]; then
+  # Create dir
+  mkdir RDS_files
+fi
