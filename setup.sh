fe07b4df08fbb6d91df5712220f34714c154587f
Linh B Ngo <lngo@wcupa.edu>
2022-01-16 11:20:53 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -29,19 +29,19 @@ do
   echo "******************************************************************************"
 done < requirements.txt
 
-if [ ! -d "data" ]; then
+if [ ! -d "user_work" ]; then
   # Create dir
-  mkdir data data/example_PBMC
+  mkdir -p user_work/data/example_PBMC
 
   # Download data
   wget https://cf.10xgenomics.com/samples/cell/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz
   tar zxvf pbmc3k_filtered_gene_bc_matrices.tar.gz
-  mv filtered_gene_bc_matrices/hg19/* data/example_PBMC
+  mv filtered_gene_bc_matrices/hg19/* user_work/data/example_PBMC
   rm -rf filtered_gene_bc_matrices pbmc3k_filtered_gene_bc_matrices.tar.gz
 fi
 
 ## RDS_files
-if [ ! -d "RDS_files" ]; then
+if [ ! -d "user_work/RDS_files" ]; then
   # Create dir
-  mkdir RDS_files
+  mkdir -p user_work/RDS_files
 fi
